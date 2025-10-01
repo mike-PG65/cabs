1a75f39acb3ef755ef2e55d5c06ee32597f8fc18
mike-PG65 <petergonye123@gmail.com>
2025-10-01 11:53:22 +0300

    cleaned up the server file

--- server/server.js
+++ server/server.js
@@ -32,11 +32,10 @@ app.use("/api/mpesa", mpesaRoutes);
 
 // ✅ Serve React frontend (client/dist)
 const __dirname1 = path.resolve(); // get project root
-app.use(express.static(path.join(__dirname1, "client", "dist")));
+app.use(express.static(path.join(__dirname1, "..", "client", "dist")));
 
-// ⚠️ FIX: use "/*" safely in Express v5
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname1, "..", "client", "dist", "index.html"));
 });
 
 // Start server after DB connection
